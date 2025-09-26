17c305c047cf615799b60f53d348c15e1615fd8e
taham8875 <taha.m8875@gmail.com>
2025-09-26 22:15:14 +0300

    Lesson 4: read from channels

--- main.go
+++ main.go
@@ -3,8 +3,47 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+
+		currentLine := ""
+
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+
+			if err != nil {
+				break
+			}
+
+			buffer = buffer[:n]
+
+			if i := bytes.IndexByte(buffer, '\n'); i != -1 {
+				currentLine += string(buffer[:i])
+				buffer = buffer[i+1:]
+				ch <- currentLine
+				currentLine = ""
+			}
+
+			currentLine += string(buffer)
+
+		}
+
+		if currentLine != "" {
+			ch <- currentLine
+		}
+	}()
+
+	return ch
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -12,32 +51,7 @@ func main() {
 		return
 	}
 
-	defer file.Close()
-
-	currentLine := ""
-
-	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
-
-		if err != nil {
-			break
-		}
-
-		buffer = buffer[:n]
-
-		if i := bytes.IndexByte(buffer, '\n'); i != -1 {
-			currentLine += string(buffer[:i])
-			buffer = buffer[i+1:]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
-
-		currentLine += string(buffer)
-
-	}
-
-	if len(currentLine) != 0 {
-		fmt.Printf("read: %s\n", currentLine)
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
